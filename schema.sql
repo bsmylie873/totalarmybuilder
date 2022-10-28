2811456726f6688400ad2388c4e897d75fc2ffe9
bsmylie873 <31117470+bsmylie873@users.noreply.github.com>
2022-10-28 10:18:29 +0100

    Update schema.sql
    Cut down for MVP implementation.

--- schema.sql
+++ schema.sql
@@ -51,61 +51,6 @@ CREATE TABLE compositions_units IF NOT EXISTS(
       REFERENCES units (id)
 )
 
-CREATE TABLE compositions_units IF NOT EXISTS(
-    composition_id INT NOT NULL,
-  	unit_id INT NOT NULL,
-  	PRIMARY KEY (composition_id, unit_key),
-  	FOREIGN KEY (composition_id)
-      REFERENCES compositions (id),
-  	FOREIGN KEY (unit_id)
-      REFERENCES units (id)
-)
-
-CREATE TABLE restrictors IF NOT EXISTS(
-    id INT INCREMENT PRIMARY KEY,
-  	name VARCHAR(50) NOT NULL,
-  	limit INT NOT NULL
-)
-
-CREATE TABLE units_restrictors IF NOT EXISTS(
-    unit_id INT NOT NULL,
-  	restrictor_id INT NOT NULL,
-  	PRIMARY KEY (unit_id, restrictor_id),
-  	FOREIGN KEY (composition_id)
-      REFERENCES compositions (id),
-  	FOREIGN KEY (unit_id)
-      REFERENCES units (id)
-)
-
-CREATE TABLE lords_restrictors IF NOT EXISTS(
-    lord_id INT NOT NULL,
-  	restrictor_id INT NOT NULL,
-  	PRIMARY KEY (lord_id, restrictor_id),
-  	FOREIGN KEY (lord_id)
-      REFERENCES lordorherocustomisation (unit_id),
-  	FOREIGN KEY (unit_id)
-      REFERENCES units (id)
-)
-
-CREATE TABLE heroes_restrictors IF NOT EXISTS(
-    hero_id INT NOT NULL,
-  	restrictor_id INT NOT NULL,
-  	PRIMARY KEY (hero_id, restrictor_id),
-  	FOREIGN KEY (hero_id)
-      REFERENCES lordorherocustomisation (unit_id),
-  	FOREIGN KEY (unit_id)
-      REFERENCES units (id)
-)
-
-CREATE TABLE heroes_restrictors IF NOT EXISTS(
-    hero_id INT NOT NULL,
-  	restrictor_id INT NOT NULL,
-  	PRIMARY KEY (hero_id, restrictor_id),
-  	FOREIGN KEY (hero_id)
-      REFERENCES lordorherocustomisation (unit_id),
-  	FOREIGN KEY (unit_id)
-      REFERENCES units (id)
-)
 
 CREATE TABLE factions IF NOT EXISTS(
     id INT INCREMENT PRIMARY KEY,
@@ -122,110 +67,16 @@ CREATE TABLE units_factions IF NOT EXISTS(
       REFERENCES factions (id)
 )
 
-CREATE TABLE lords_factions IF NOT EXISTS(
-    lord_id INT NOT NULL,
-  	faction_id INT NOT NULL,
-  	PRIMARY KEY (lord_id, faction_id),
-  	FOREIGN KEY (lord_id)
-      REFERENCES lordorherocustomisation (id),
-  	FOREIGN KEY (faction_id)
-      REFERENCES factions (id)
-)
-
-CREATE TABLE heroes_factions IF NOT EXISTS(
-    hero_id INT NOT NULL,
-  	faction_id INT NOT NULL,
-  	PRIMARY KEY (hero_id, faction_id),
-  	FOREIGN KEY (hero_id)
-      REFERENCES lordorherocustomisation (id),
-  	FOREIGN KEY (faction_id)
-      REFERENCES factions (id)
-)
-
-CREATE TABLE attributes IF NOT EXISTS(
+CREATE TABLE lords IF NOT EXISTS(
     id INT INCREMENT PRIMARY KEY,
-  	name VARCHAR(50) NOT NULL,
-  	limit INT NOT NULL,
-)
-
-CREATE TABLE units_attributes IF NOT EXISTS(
-    unit_id INT NOT NULL,
-  	attribute_id INT NOT NULL,
-  	PRIMARY KEY (unit_id, attribute_id),
+  	unit_id INT NOT NULL,
   	FOREIGN KEY (unit_id)
-      REFERENCES units (id),
-  	FOREIGN KEY (attribute_id)
-      REFERENCES attributes (id)
+      REFERENCES units (id)
 )
 
-CREATE TABLE lords_attributes IF NOT EXISTS(
-    lord_id INT NOT NULL,
-  	attribute_id INT NOT NULL,
-  	PRIMARY KEY (lord_id, attribute_id),
-  	FOREIGN KEY (lord_id)
-      REFERENCES lordorherocustomisation (id),
-  	FOREIGN KEY (attribute_id)
-      REFERENCES attributes (id)
-)
-
-CREATE TABLE heroes_attributes IF NOT EXISTS(
-    hero_id INT NOT NULL,
-  	attribute_id INT NOT NULL,
-  	PRIMARY KEY (hero_id, attribute_id),
-  	FOREIGN KEY (hero_id)
-      REFERENCES lordorherocustomisation (id),
-  	FOREIGN KEY (attribute_id)
-      REFERENCES attributes (id)
-)
-
-CREATE TABLE lordorherocustomisation IF NOT EXISTS(
-    unit_id INT NOT NULL,
-  	spell_id INT,
-  	ability_id INT,
-  	item_id INT,
-  	mount_id INT,
-  	PRIMARY KEY (unit_id, spell_id, ability_id, item_id, mount_id),
+CREATE TABLE heroes IF NOT EXISTS(
+    id INT INCREMENT PRIMARY KEY,
+  	unit_id INT NOT NULL,
   	FOREIGN KEY (unit_id)
-      REFERENCES units (id),
-  	FOREIGN KEY (spell_id)
-      REFERENCES spells (id),
-  	FOREIGN KEY (ability_id)
-      REFERENCES abilites (id),
-  	FOREIGN KEY (item_id)
-      REFERENCES items (id)
-  	FOREIGN KEY (mount_id)
-      REFERENCES mounts (id)
-)
-
-CREATE TABLE spells IF NOT EXISTS(
-    id INT INCREMENT PRIMARY KEY,
-  	name VARCHAR(50) NOT NULL,
-  	description VARCHAR(255) NOT NULL,
-  	cost INT NOT NULL,
-  	lore ENUM NOT NULL,
-)
-
-CREATE TABLE abilities IF NOT EXISTS(
-    id INT INCREMENT PRIMARY KEY,
-  	name VARCHAR(50) NOT NULL,
-  	description VARCHAR(255) NOT NULL,
-  	cost INT NOT NULL,
-  	passive BOOLEAN NOT NULL,
-)
-
-CREATE TABLE items IF NOT EXISTS(
-    id INT INCREMENT PRIMARY KEY,
-  	name VARCHAR(50) NOT NULL,
-  	description VARCHAR(255) NOT NULL,
-  	cost INT NOT NULL,
-  	passive BOOLEAN NOT NULL,
-)
-
-CREATE TABLE mounts IF NOT EXISTS(
-    id INT INCREMENT PRIMARY KEY,
-  	name VARCHAR(50) NOT NULL,
-  	description VARCHAR(255) NOT NULL,
-  	cost INT NOT NULL,
-  	flying BOOLEAN NOT NULL,
-)
-;
+      REFERENCES units (id)
+);
