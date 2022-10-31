5aa3529b79591de5f443002e3232e32be7d527c2
Brandon Smylie <brandon.smylie@unosquare.com>
2022-10-31 14:07:29 +0000

    Initialised MVC application repo

---
+++ TotalArmyBuilder/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TotalArmyBuilder.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
