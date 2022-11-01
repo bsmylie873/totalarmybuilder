b12a65d0ce8ee546547ec292e4010a54eb5fbed6
Brandon Smylie <brandon.smylie@unosquare.com>
2022-11-01 14:44:15 +0000

    Removed defunct repo

--- TotalArmyBuilder/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace TotalArmyBuilder.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
