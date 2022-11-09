7044a5293dea311605ade0dc8d6ce896a51cd4ad
Brandon Smylie <brandon.smylie@unosquare.com>
2022-11-09 17:14:39 +0000

    DTO added

---
+++ server/src/TotalArmyBuilder.Services/DTOs/AccountDTO.cs
@@ -0,0 +1,10 @@
+namespace TotalArmyBuilder.Service.DTOs;
+
+public class AccountDTO
+{
+    public int Id { get; set; }
+    
+    public string Username { get; set; }
+    
+    public string Email { get; set; }
+}
