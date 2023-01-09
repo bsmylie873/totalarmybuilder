57cd0d74d100b0ab019968e6be270bfd61b3ab06
Brandon Smylie <brandon.smylie@unosquare.com>
2023-01-09 16:11:21 +0000

    Major UI changes, WIP.

--- ui/src/pages/search/index.tsx
+++ ui/src/pages/search/index.tsx
@@ -1,7 +1,17 @@
+import { Container, Grid, Button, Box } from "@mui/material";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import CompositionGrid from "../../components/composition_grid";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 
 const Search = () => {
-    return (<div>Search</div>)
-}
+  return (
+    <>
+      <div>
+        <CompositionGrid></CompositionGrid>
+      </div>
+    </>
+  );
+};
 
 export default Search;
